69a7e4a8fb25f6fc7805059f18efde368f155648
Prasun_Seal <prasun.seal@cognizant.com>
2020-04-02 22:35:49 +0530

    3rd assignment

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,30 @@
 package io.pivotal.pal.tracker;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
+	//@Value("${welcome.message}")
+	public String massage;
+	
+	
+	public WelcomeController(@Value("${welcome.message}") String massage) {
+		// TODO Auto-generated constructor stub
+		this.massage=massage;
+		System.out.println("massage-------"+massage);
+		//WELCOME_MESSAGE="hello";
+	}
+	public WelcomeController() {
+		
+	}
+	
+
+	@GetMapping("/")
     public String sayHello() {
         return "hello";
     }
